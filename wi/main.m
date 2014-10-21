3429d77b188236a534ae5e14672a36fb1f689fd1
willowsys <willowsys@willowsystekiMac.local>
2014-10-21 22:50:01 +0800

    Initial Commit

---
+++ wi/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wi
+//
+//  Created by willowsys on 2014/10/21.
+//  Copyright (c) 2014年 willowsys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
